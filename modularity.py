c60175bcfec7cf786f8bc2074b8fab8a6aa5cc23
Jimmy Vineyard <72844296+jwvineyard@users.noreply.github.com>
2023-02-12 20:30:51 -0600

    Add files via upload
    This file is not yet fully working. The problem is with the NetworkX adjacency_matrix() function returning a sparse SciPy array, while the documentation says it should return a 2d NumPy array.
    
    This implements the algorithm for splitting a network based on the specified paper. It will soon be updated to include splitting networks into more than 2 communities.

---
+++ modularity.py
@@ -0,0 +1,64 @@
+import numpy as np
+from numpy import linalg as LA
+import pandas as pd
+import networkx as nx
+import scipy as sp
+
+# This algorithm is taken from "The Method of Optimal Modularity" section of the PNAS paper
+
+
+# Equation 2
+# s is a column vector with s[i] being 1 if node i is in group 1, and -1 if node i is in group 2
+# B is the modulatiry matrix, gained from equation 3
+def Q(s,G,B):
+    m = G.number_of_edges
+    return (1/(4*m)) * np.matmul(np.transpose(s), (np.matmul(B, s)))
+
+
+# Equation 3
+def modularity_matrix(G):
+    # populate modularity matrix
+    m = G.number_of_edges()
+    B = np.zeros((m,m))
+    A = nx.adjacency_matrix(G).toarray
+    for i in np.arange(m):
+        for j in np.arange(m):
+            ki = G.degree[i]
+            kj = G.degree[j]
+            B[i][j] = A[i][j] - ((ki*kj)/(2*m))
+    return B
+
+# Equation 4
+# Note: if at any point a proposed split makes a zero or negative contribution to the total modularity, do not divide.
+
+
+# Algorithm instructions:
+def modularity(G):
+    # Construct the modularity matrix (equation 3) for the network
+    B = modularity_matrix(G)
+    # Find the MMs most positive eigenvalue & corresponding eigenvector
+    w,v = np.linalg.eig(B)
+    ''' 
+    # rank counts the number of positive values in an eigenvector
+    # Don't think I actually need this, I will potentially remove it later
+    rank = np.zeros(w.size)
+    for i, vect in enumerate(v):
+        for k in vect:
+            if k >= 0:
+                rank[i] += 1
+    '''
+    # u is the eigenvector with the most positive elements
+    u = v[np.argmax(w)]
+    # divides the graph according to the leading eigenvector
+    s = np.ones(u.size)
+    for i in u:
+        if u[i] < 0: s[i] = -1
+
+    return Q(s,G,B)
+print(modularity(nx.karate_club_graph()))
+
+# Divide the network into two parts according to the signs of the elements of the eigenvector
+
+# Repeat this process for each of the parts, using the generalized modularity matrix (equation 6)
+
+# End.
